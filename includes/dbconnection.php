3972ea5fedff4cc1b2873366a0a5a67f51acf524
KDWeerasuriya <dilshankelum@gmail.com>
2021-07-01 16:59:35 +0530

    change db connection  file

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -3,7 +3,7 @@
 $serverName = "localhost";
 $userName = "root";
 $password = "";
-$dbName = "ramifyohotel1";
+$dbName = "ramifyohotel";
 
 $db = mysqli_connect($serverName, $userName, $password, $dbName);
 mysqli_report(MYSQLI_REPORT_OFF);
