308b48b484e62b177aa1d1a3a2583711f9e261ac
KDWeerasuriya <dilshankelum@gmail.com>
2021-07-08 09:38:27 +0530

    added room avalability

--- admindashbord.php
+++ admindashbord.php
@@ -98,27 +98,61 @@ if(!isset($_SESSION['email'])){
     <h4 style="text-align: center;"><b>Room Availability</b></h4>
 		  <div class="card-action">
 		    <div class="card-body">
-		 
-		    <div class="row m-0 row-group text-center border-top border-light-3">
+	
 
+     <?php
+     $uavroomcount = 0;
+     $avroomcount = 0;
+                $Result = mysqli_query($db,"SELECT * FROM room");
+                while($row=mysqli_fetch_array($Result)){
+
+                  $roomname = $row["roomno"];
+                  $roomstatus = $row["roomstatus"];
+                  $roomid = $row["roomid"];
+                 
+                    if($roomstatus==0){
+                      
+                      ?> 
+                      
+                      <button type="button" class="btn btn-success"> <?php echo "$roomname";?> <br>Avalible </button>
+                   
+                      <?php 
+                     $avroomcount++; 
+                    }        
+                
+                    elseif($roomstatus==1){
+
+                     
+               ?>     
+                  <button type="button" class="btn btn-danger"><?php echo "$roomname";?><br>Unavalible </button>
+                  <?php 
+                   $uavroomcount++;
+                }    } 
+              
+
+               ?>
+<br>
+     
+		    <div class="row m-0 row-group text-center border-top border-light-3">
+<br>
 		      <div class="col-12 col-lg-4">
 		        <div class="p-3">
-		          <h5 class="mb-0">12</h5>
-			          <small class="mb-0">Overall Booked Rooms <span> <i class="fa fa-arrow-up"></i> 2.43%</span></small>
+		          <h5 class="mb-0"><?php echo" $uavroomcount";?> </h5>
+			          <small class="mb-0">Overall Booked Rooms <span><?php $a=($uavroomcount/20)*100; echo"$a";?>%</span></small>
 		        </div>
 		      </div>
 
           <div class="col-12 col-lg-4">
             <div class="p-3">
-              <h5 class="mb-0">20</h5>
-                <small class="mb-0">Avalibles Room Count <span> <i class="fa fa-arrow-up"></i> 12.65%</span></small>
+              <h5 class="mb-0"><?php echo "$avroomcount";?></h5>
+                <small class="mb-0">Avalibles Room Count <span> <?php $a=($avroomcount/20)*100; echo"$a";?>%</span></small>
             </div>
           </div>
 
           <div class="col-12 col-lg-4">
             <div class="p-3">
-              <h5 class="mb-0">30</h5>
-                <small class="mb-0">Total Rooms <span> <i class="fa fa-arrow-up"></i> 5.62%</span></small>
+              <h5 class="mb-0">20</h5>
+                <small class="mb-0">Total Rooms </small>
             </div>
           </div>
 
