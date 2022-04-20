928c7709b28775e379cf35b2f70bffa89c0c653a
SameeraGurusinghe <sameeraudayanga1996@gmail.com>
2022-04-20 10:53:07 +0530

    terms updated

--- termsandcondition.php
+++ termsandcondition.php
@@ -20,12 +20,12 @@
 
     <h4>TERMS AND CONDITIONS</h4>
 
-    <p>Last updated January 01, 2021</p>
+    <p>Last updated on January 01, 2022</p>
     
     
     <h5>AGREEMENT TO TERMS</h5>
     
-    <h6>These Terms of Use constitute a legally binding agreement made between you, whether personally or on behalf of an entity (“you”) and Ramifyo Coco Beach Resort Pvt (Ltd) ("Company", “we”, “us”, or “our”), concerning your access to and use of the https://www.ramifyococobeachresort.lk website as well as any other media form, media channel, mobile website or mobile application related, linked, or otherwise connected thereto (collectively, the “Site”). We are registered in Sri Lanka and have our registered office at No: 155, Poruthota Road, Palagathure, Kochchikade, Negombo 11540. You agree that by accessing the Site, you have read, understood, and agreed to be bound by all of these Terms of Use. IF YOU DO NOT AGREE WITH ALL OF THESE TERMS OF USE, THEN YOU ARE EXPRESSLY PROHIBITED FROM USING THE SITE AND YOU MUST DISCONTINUE USE IMMEDIATELY.
+    <h6>These Terms of Use constitute a legally binding agreement made between you, whether personally or on behalf of an entity (“you”) and Ramifyo Coco Beach Resort Pvt (Ltd) ("Company", “we”, “us”, or “our”), concerning your access to and use of the <a href="http://www.ramifyoresort.byethost24.com">http://www.ramifyoresort.byethost24.com</a> website as well as any other media form, media channel, mobile website or mobile application related, linked, or otherwise connected thereto (collectively, the “Site”). We are registered in Sri Lanka and have our registered office at No: 155, Poruthota Road, Palagathure, Kochchikade, Negombo 11540. You agree that by accessing the Site, you have read, understood, and agreed to be bound by all of these Terms of Use. IF YOU DO NOT AGREE WITH ALL OF THESE TERMS OF USE, THEN YOU ARE EXPRESSLY PROHIBITED FROM USING THE SITE AND YOU MUST DISCONTINUE USE IMMEDIATELY.
     
     Supplemental terms and conditions or documents that may be posted on the Site from time to time are hereby expressly incorporated herein by reference. We reserve the right, in our sole discretion, to make changes or modifications to these Terms of Use at any time and for any reason. We will alert you about any changes by updating the “Last updated” date of these Terms of Use, and you waive any right to receive specific notice of each such change. Please ensure that you check the applicable Terms every time you use our Site so that you understand which Terms apply. You will be subject to, and will be deemed to have been made aware of and to have accepted, the changes in any revised Terms of Use by your continued use of the Site after the date such revised Terms of Use are posted.
     
